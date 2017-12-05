c8a8f113ba721a0a2341e07437e3758a46cea701
Nicolas Gotchac <ngotchac@gmail.com>
2017-12-05 18:30:59 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const Koa = require('koa');
+const Body = require('koa-body');
+const Cors = require('@koa/cors');
+const Logger = require('koa-logger');
+const Router = require('koa-router');
+const Static = require('koa-static');
+
+const fs = require('fs-extra');
+const os = require('os');
+const path = require('path');
+const uuid = require('uuid/v4');
+
+const app = new Koa();
+const router = new Router();
+
+const TMP_DIR = path.join(os.tmpdir(), '.storage');
+
+fs.ensureDir(TMP_DIR);
+
+app.use(Logger());
+app.use(Cors());
+app.use(Body({ multipart: true }));
+app.use(Static(path.join(__dirname, '/public')));
+
+app.use(async (ctx, next) => {
+  const fpath = path.join(TMP_DIR, ctx.path);
+  const fstat = await fs.stat(fpath);
+
+  if (fstat.isFile()) {
+    ctx.type = path.extname(fpath);
+    ctx.body = fs.createReadStream(fpath);
+  } else {
+    await next();
+  }
+});
+
+router.post('/', async (ctx) => {
+  const { file } = ctx.request.body.files;
+  const reader = fs.createReadStream(file.path);
+  const filepath = `${uuid()}/${file.name}`;
+  const fullpath = path.join(TMP_DIR, filepath);
+
+  await fs.ensureFile(fullpath);
+
+  const stream = fs.createWriteStream(fullpath);
+
+  reader.pipe(stream);
+  console.log('uploading %s -> %s', file.name, stream.path);
+
+  ctx.body = {
+    link: filepath
+  };
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+const PORT = process.env.PORT || process.env.NODE_PORT || 3000;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
